fa1cd9d29e99f8a4b79e89dfc7932d0be9df30d2
gregbkatz <gregbkatz@gmail.com>
2021-02-08 19:44:39 -0800

    clean up and more efficient

--- coins.py
+++ coins.py
@@ -11,24 +11,18 @@ def iter(table, coins, target, start, end):
   t_start = time.time()
   L = max(coins)
   n_coins = len(coins)
-  end = min(target+1, end)
-  for i in range(start, end):
+  end = min(target+coins[1], end)
+  for i in range(start, end, coins[1]):
     row = i % L
-    curr = [0] * n_coins
-    for j, coin in enumerate(coins):
-      if i >= coin:
-        curr[j] = table[(i-coin) % L][j]
+    for j in range(1, n_coins):
+      shift = (i-coins[j])%L
+      table[row][j] = table[row][j-1] + table[shift][j]
 
-    table[row][n_coins-1] = curr[n_coins-1]
-    for j in range(n_coins-2, -1, -1):
-      table[row][j] = table[row][j+1] + curr[j]
-
-  i = end - 1
-  fname = "coins.p".format(i)
+  i = end - coins[1]
+  fname = "coins2.p".format(i)
   pickle.dump({"i": i, "table": table}, open( fname, "wb" ) )
   duration = time.time() - t_start
-  print(i, ":", table[(i)%L][0], round(10*duration)/10, flush=True)
-
+  print(i, ":", table[i%L][-1], round(10*duration)/10, flush=True)
   return table
 
 def initialize(coins, target):
@@ -37,26 +31,28 @@ def initialize(coins, target):
   table = []
   for i in range(L+1):
     curr = [0]*n_coins
+    curr[0] = 1
     table.append(curr)
   for i in range(n_coins):
     table[0][i] = 1
   return table
 
 def method(coins, target, pickle_file=None):
+  coins = sorted(coins)
   if pickle_file:
     p = pickle.load( open( pickle_file, "rb" ) )
     table = p['table']
     i = p['i'] + 1
   else:
     table = initialize(coins, target)
-    i = 1
+    i = coins[1]
 
   while i < target + 1:
     table = iter(table, coins, target, i, i+step)
     i += step
 
   L = max(coins)
-  print(table[target % L][0])
+  print(table[target % L][-1])
 
 # method(coins, target)
-method(coins, target, "coins.p")
+method(coins, target, "coins2.p")
